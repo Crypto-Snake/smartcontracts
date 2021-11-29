97d9d97c163710589161dfab44d8a3996355d786
Gendalf The Gray <gorobchik173@gmail.com>
2021-11-29 20:47:15 +0200

    ADD | Add lower admin role for providing signatures for updating stats.

--- contracts/utils/Allowable.sol
+++ contracts/utils/Allowable.sol
@@ -5,15 +5,29 @@ pragma solidity ^0.8.0;
 import "./Ownable.sol";
 
 abstract contract Allowable is Ownable {
+    address private _lowerAdmin;
     mapping(address => bool) public allowedAddresses;
+
+    function lowerAdmin() public view returns (address) {
+        return _lowerAdmin;
+    }
 
     modifier onlyAllowedAddresses {
         require(allowedAddresses[msg.sender] || msg.sender == owner(), "Allowable: Not allowed address");
         _;
     }
 
+    modifier onlyOwnerOrLowerAdmin {
+        require(msg.sender == owner() || msg.sender == lowerAdmin(), "Allowable: Not allowed address");
+        _;
+    }
+
     function updateAllowedAddresses(address _address, bool _value) external virtual onlyOwner {
         allowedAddresses[_address] = _value;
+    }
+
+    function updateLowerAdmin(address _address) external virtual onlyOwner {
+        _lowerAdmin = _address;
     }
 }
 
