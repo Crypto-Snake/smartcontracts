6cb956b5571e34f358f32d4faaa872d2c67a29f7
Gendalf The Gray <gorobchik173@gmail.com>
2021-12-06 18:10:30 +0200

    REF | Update RescueManager contract payable method signature.

--- contracts/utils/RescueManager.sol
+++ contracts/utils/RescueManager.sol
@@ -12,7 +12,7 @@ abstract contract RescueManager is Ownable {
     event Rescue(address indexed receiver, uint amount);
     event RescueToken(address indexed receiver, address indexed token, uint amount);
 
-    function rescue(address payable _to, uint256 _amount) external onlyOwner {
+    function rescueBNB(address payable _to, uint256 _amount) external onlyOwner {
         require(_to != address(0), "RescueManager: Cannot rescue to 0x0");
         require(_amount > 0, "RescueManager: Cannot rescue 0");
 
