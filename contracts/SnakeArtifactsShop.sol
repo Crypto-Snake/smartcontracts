56f2e38e7d6425c0417b3a20149d3e8b7d07db90
Gendalf The Gray <gorobchik173@gmail.com>
2022-03-22 18:56:30 +0200

    ADD | Add snake token burning after artifact purchase.

--- contracts/SnakeArtifactsShop.sol
+++ contracts/SnakeArtifactsShop.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 import "./storages/SnakeArtifactsShopStorage.sol";
+import "./interfaces/IBEP20.sol";
 
 contract SnakeArtifactsShop is SnakeArtifactsShopStorage {
 
@@ -22,11 +23,18 @@ contract SnakeArtifactsShop is SnakeArtifactsShopStorage {
         require(count > 0, "ArtifactsShop: Artifacts count must be greater than 0");
         require(allowedTokens[purchaseToken], "ArtifactsShop: Buying artifacts for this token is not allowed");
         uint artifactPrice = nftManager.getArtifactProperties(id).Price;
-        uint price = whitelist[msg.sender] && id > 10 && id < 26 ? artifactPrice / 2 : artifactPrice;
+        uint price = whitelist[msg.sender] && id > 10 && id < 24 ? artifactPrice / 2 : artifactPrice;
         require(price > 0, "ArtifactsShop: Artifact not found or no corresponding pair");
         
         uint finalPrice = price * count;
-        TransferHelper.safeTransferFrom(purchaseToken, msg.sender, custodian, finalPrice);
+        
+        if(purchaseToken == snakeToken) {
+            TransferHelper.safeTransferFrom(purchaseToken, msg.sender, address(this), finalPrice);
+            IBEP20(snakeToken).burn(finalPrice);
+        } else {
+            TransferHelper.safeTransferFrom(purchaseToken, msg.sender, custodian, finalPrice);
+        }
+
         artifactsNFT.safeTransferFrom(artifactsOwner, msg.sender, id, count, "0x");
         emit BuyArtifact(msg.sender, id, purchaseToken, count, finalPrice);
     }
--- contracts/interfaces/IBEP20.sol
+++ contracts/interfaces/IBEP20.sol
@@ -9,4 +9,5 @@ interface IBEP20 {
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function burn(uint256 amount) external;
 }
